ab1364791daa917b67d54df8a511326b7ca188f3
ArieleSilva <ariele.silva1@marist.edu>
2016-09-06 22:57:04 -0400

    Lab 1

---
+++ lab 1/SumDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumDigits {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("enter a number between 0 and 1000: ");
+		int number = input.nextInt();
+			int onesPlace = number % 10;		
+			number /= 10;					
+				int tensPlace = number % 10;		
+				number /= 10;				
+					int hundredsPlace = number % 10;	
+					number /= 10;				
+						int total = hundredsPlace + tensPlace + onesPlace;
+		System.out.println("The total of the numbers are " + total);
+	}
+}
+
+//silvs code
