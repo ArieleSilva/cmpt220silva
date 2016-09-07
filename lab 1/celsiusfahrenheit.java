1066d89acb5df07ca70039693d64c07f647c644c
ArieleSilva <ariele.silva1@marist.edu>
2016-09-06 21:49:31 -0400

    Lab 1

---
+++ lab 1/celsiusfahrenheit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CelsiusFahrenheit {
+	public static void main(String[] arg){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input Celsius Degree: ");
+		double celsius = input.nextDouble();
+		celsius = ((9.0 / 5) * celsius) + 32;
+		System.out.println("Your temperture in Fahrenheit is: " + celsius);
+		
+	}
+}
+
+//silvs code
