e950ad8fa0b6aa8787415df85a1971646b7acee2
ArieleSilva <ariele.silva1@marist.edu>
2016-09-28 20:58:39 -0400

    Lab 4 Updated

--- lab 4/6.8/CelciusFarenheit2.java
+++ lab 4/6.8/CelciusFarenheit2.java
@@ -5,15 +5,10 @@
  */
 public class CelciusFarenheit2 {
   public static void main(String[] args){
-    System.out.format("  Celsius Farenheit | 
-	Farenheit  Celsius\n");
-    for (double cel=40.0 , far = 120; cel > 30;
-	cel -= 1, far-=10)    {
+    System.out.format("  Celsius Farenheit | Farenheit  Celsius\n");
+    for (double cel=40.0 , far = 120; cel > 30; cel -= 1, far-=10)    {
       double[] myList = new double[100];
-      System.out.format("%9.1f %9.1f | 
-	  %9.1f %9.1f\n", cel, Cel2Far(cel), 
-	  far, Far2Cel(far)) ;
-
+      System.out.format("%9.1f %9.1f | %9.1f %9.1f\n", cel, Cel2Far(cel), far, Far2Cel(far)) ;
     }
   }
   public static double Cel2Far(double cel){
